27ef174201f3c00a62313b555781f4fbf1c6a82f
小林駿太 <shunta@MacBook-Pro.local>
2018-11-07 22:26:15 +0900

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,36 @@
           password: password)
 end
 
+User.create!(name: "サンプルユーザー",
+        full_name: "サンプルユーザー",
+        email: "sample@email.com",
+        accepted: true,
+        password: password)
+
+User.create!(name: "サンプルユーザー2",
+        full_name: "サンプルユーザー2",
+        email: "sample2@email.com",
+        accepted: true,
+        password: password)
+
+
 users = User.all
 users.each do |user|
-  post = user.posts.create(caption: "")
-  post.photos.create(image: open("#{Rails.root}/db/fixtures/cat.jpeg"))
+  post1 = user.posts.create(caption: "ネコ")
+  post1.photos.create(image: open("#{Rails.root}/db/fixtures/cat.jpeg"))
+
+  post2 = user.posts.create(caption: "ネコ２")
+  post2.photos.create(image: open("#{Rails.root}/db/fixtures/cat2.jpeg"))
+
+  post3 = user.posts.create(caption: "シマウマ")
+  post3.photos.create(image: open("#{Rails.root}/db/fixtures/hourse.jpeg"))
+
+  post1 = user.posts.create(caption: "イヌ")
+  post1.photos.create(image: open("#{Rails.root}/db/fixtures/dog.jpeg"))
+
+  post1 = user.posts.create(caption: "ハリネズミ")
+  post1.photos.create(image: open("#{Rails.root}/db/fixtures/mouse.jpeg"))
+
+  post1 = user.posts.create(caption: "ライオン")
+  post1.photos.create(image: open("#{Rails.root}/db/fixtures/lion.jpeg"))
 end
