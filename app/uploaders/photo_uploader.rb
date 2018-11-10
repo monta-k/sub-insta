5299c3c6f3ffc76bdc833f6207bfe6c4dec361d0
小林駿太 <shunta@MacBook-Pro.local>
2018-11-10 23:27:28 +0900

    photo_uploader fix

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -5,7 +5,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
-    ftorage :fog
+    storage :fog
   else
     storage :file
   end
